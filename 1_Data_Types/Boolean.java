01597ce865e7cc6cbf81b723e8d8c24c16c3de65
Venkatesh-Maley <127150040+Venkatesh-Maley@users.noreply.github.com>
2023-06-28 22:41:28 +0530

    Create Boolean.java

---
+++ 1_Data_Types/Boolean.java
@@ -0,0 +1,18 @@
+//Java Program to Demonstrate Boolean Primative Data types
+
+class Datatype{
+    public static void main(String args[])
+    {
+        boolean a=false;
+        boolean b=true;
+
+        if(b==true){
+            System.out.println("Hi Geek");
+        }
+
+        if(a==false){
+            System.out.println("Hello Geek");
+
+        }
+    }
+}
