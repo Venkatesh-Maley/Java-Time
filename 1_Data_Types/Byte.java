b3d95c9098f08fa5d7eb8c92e1ee7401c1b36fbd
Venkatesh-Maley <127150040+Venkatesh-Maley@users.noreply.github.com>
2023-06-29 00:01:57 +0530

    Add files via upload

---
+++ 1_Data_Types/Byte.java
@@ -0,0 +1,18 @@
+//Java Program to Demonstrate Byte Data types
+
+class Datatype{
+    public static void main(String args[]){
+        
+        byte a=126;
+        System.out.println(a);
+
+        a++;
+        System.out.println(a);
+
+        a++;
+        System.out.println(a);
+
+        a++;
+        System.out.println(a);
+    }
+}
