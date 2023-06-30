cad8647ff9b1b1422d155c059fa9d29e9c0a1113
Venkatesh-Maley <127150040+Venkatesh-Maley@users.noreply.github.com>
2023-06-30 23:16:18 +0530

    Add files via upload

---
+++ 1_Data_Types/Datatypes.java
@@ -0,0 +1,23 @@
+// Java Program to Demonstrate All other Primitive Data Types
+
+class Datatype{
+    public static void main(String args[])
+    {
+        char a='V';
+        int i=143;
+        byte b=4;
+        short s=420;
+        double d= 4.3556753;
+        float f= 4.75092893f;
+        long l=121212;
+
+        System.out.println("char:" +a);
+        System.out.println("integer"+i);
+        System.out.println("byte:"+b);
+        System.out.println("short:"+s);
+        System.out.println("float:"+f);
+        System.out.println("double:"+d);
+        System.out.println("long:"+l);
+
+    }
+}
