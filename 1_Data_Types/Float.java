53f4cf01b52a7f195c652f07b2a06e6986c93e8b
Venkatesh-Maley <127150040+Venkatesh-Maley@users.noreply.github.com>
2023-07-01 16:42:42 +0530

    Add files via upload

---
+++ 1_Data_Types/Float.java
@@ -0,0 +1,11 @@
+//Java Program to Illustrate Float Primative Data types
+
+class Datatype{
+    public static void main(String[]args)
+
+    {
+        float value=9.8f;
+        System.out.println(value);
+        
+    } 
+}
